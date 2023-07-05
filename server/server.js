55ca82c3a0d544105f8f9ac33f9d2b8b57413329
Jordan <jpessman22@gmail.com>
2023-07-05 08:53:49 -0400

    Adds templated api routes

--- server/server.js
+++ server/server.js
@@ -6,12 +6,71 @@ const app = express();
 
 app.use(express.json()); //Parse JSON body 
 
-app.get("/planets", function (req, res) {
+app.get("/api/characters", function (req, res) {
     findAllPlanets((planets) => {
         res.status(200).send(planets);
     })
 });
 
+app.get("/api/films", function (req, res) {
+    findAllPlanets((planets) => {
+        res.status(200).send(planets);
+    })
+});
+
+app.get("/api/planets", function (req, res) {
+    findAllPlanets((planets) => {
+        res.status(200).send(planets);
+    })
+});
+
+app.get("/api/characters/:id", function (req, res) {
+    findAllPlanets((planets) => {
+        res.status(200).send(planets);
+    })
+});
+
+app.get("/api/films/:id", function (req, res) {
+    findAllPlanets((planets) => {
+        res.status(200).send(planets);
+    })
+});
+
+app.get("/api/planets/:id", function (req, res) {
+    findAllPlanets((planets) => {
+        res.status(200).send(planets);
+    })
+});
+
+app.get("/api/films/:id/characters", function (req, res) {
+    findAllPlanets((planets) => {
+        res.status(200).send(planets);
+    })
+});
+
+app.get("/api/films/:id/planets", function (req, res) {
+    findAllPlanets((planets) => {
+        res.status(200).send(planets);
+    })
+});
+
+app.get("/api/characters/:id/films", function (req, res) {
+    findAllPlanets((planets) => {
+        res.status(200).send(planets);
+    })
+});
+
+app.get("/api/planets/:id/films", function (req, res) {
+    findAllPlanets((planets) => {
+        res.status(200).send(planets);
+    })
+});
+
+app.get("/api/planets/:id/characters", function (req, res) {
+    findAllPlanets((planets) => {
+        res.status(200).send(planets);
+    })
+});
 
 console.log("server starting on port: " + port );
 app.listen(port);
